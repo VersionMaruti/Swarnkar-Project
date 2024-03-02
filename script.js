449a3ff669270579c49fda39e441eb44ce91d526
DS-ALGO <sonihero01@gmail.com>
2024-03-02 19:54:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementById('nav-toggle');
+const navlinks = document.getElementById('nav-links');
+
+toggleButton.addEventListener('click', () => {
+    navlinks.classList.toggle('active');
+})
