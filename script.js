dfa3ab72559a3d0ab8836bc7d38de5597af1116a
Himanshi20021998 <135860725+Himanshi20021998@users.noreply.github.com>
2024-03-03 17:16:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-const toggleButton = document.getElementById('nav-toggle');
-const navlinks = document.getElementById('nav-links');
+document.addEventListener('DOMContentLoaded', function(){
+    function handleAnchorClick(event){
+        event.preventDefault();
+        window.location.href = event.target.href;
+    }
 
-toggleButton.addEventListener('click', () => {
-    navlinks.classList.toggle('active');
-})
+    function handleButtonClick(event){
+        console.log('Button clicked!');
+    }
+
+    var anchorTags = document.querySelectorAll('a');
+    var buttons = document.querySelectorAll('button');
+
+    anchorTags.forEach(function(anchor){
+        anchor.addEventListener('click', handleAnchorClick);
+    });
+
+    buttons.forEach(function(button){
+        button.addEventListener('click', handleButtonClick);
+    });
+});
